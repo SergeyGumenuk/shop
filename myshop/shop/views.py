c3d35367dd35a5c973457ed562ae5add0067e9f9
Sergey Gumenyuk <audi-1988@inbox.ru>
2022-12-26 19:54:19 +0700

    add a functionality to add/remove products to/from the cart. add the cart detail template

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 
+from cart.forms import CartAddProductForm
 from shop.models import Product, Category
 
 
@@ -23,6 +24,8 @@ def product_detail(request, id, slug):
                                 pk=id,
                                 slug=slug,
                                 available=True)
+    cart_product_form = CartAddProductForm()
     return render(request,
                   'shop/product/detail.html',
-                  {'product': product})
+                  {'product': product,
+                   'cart_product_form': cart_product_form})
